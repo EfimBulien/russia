e35aa8069020f70368063472c03d8108e8235d2a
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-01-12 19:04:54 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	cout << "Привет\a мир!";
+	cout << "Привет\b мир!";
+	cout << "Привет\t мир!";
+	cout << "Привет\n мир!";
+	cout << "Привет\v мир!";
+	cout << "Привет\r мир!";
+	cout << "Привет\f мир!";
+	cout << "Привет\" мир!";
+	cout << "Привет\' мир!";
+	cout << "Привет\\ мир!";
+
+}
+*/
